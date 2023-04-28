7295047c54d7b706f080934692790787038fca52
Harbour% <2358272468@qq.com>
2023-04-28 10:53:56 +0800

    feat: 合并代码

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,6 @@
 <template>
   <!-- <videoPlayer ref="videoPlayer" :options="playerOptions" class="vjs-custom-skin videoPlayer" :playsinline="true" />-->
-  <img id="img"
-    src="http://152.136.213.16:8001/video/pull" />
+  <img id="img" src="http://152.136.213.16:8001/video/pull" />
 
   <!-- <canvas id='test_canvas' width='640px' height='480px' style='border:1px solid #d3d3d3'>
   </canvas> -->
@@ -30,19 +29,54 @@ import { onMounted } from 'vue';
 //     form.submit();
 // }
 
-const nbMethod = () => {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'http://152.136.213.16:8001/video/pull', true);
-  xhr.responseType = 'blob';
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      // 处理数据流
-      var blob = xhr.response;
-      var url = URL.createObjectURL(blob);
-      document.getElementById('img').src = url;
+// const nbMethod = () => {
+//   var xhr = new XMLHttpRequest();
+//   xhr.open('GET', 'http://152.136.213.16:8001/video/pull', true);
+//   xhr.responseType = 'blob';
+//   xhr.onreadystatechange = function () {
+//     if (xhr.readyState == 4 && xhr.status == 200) {
+//       // 处理数据流
+//       var blob = xhr.response;
+//       var url = URL.createObjectURL(blob);
+//       document.getElementById('img').src = url;
+//     }
+//   };
+//   xhr.send();
+// }
+
+var reader = null
+var stream = null
+var SOI = new Uint8Array([0xff, 0xd8])
+const indexOfSOI = (array) => {
+  for (let i = 0; i < array.length - 1; i++) {
+    if (
+      array[i] === SOI[0] &&
+      array[i + 1] === SOI[1]
+    ) {
+      return i;
     }
-  };
-  xhr.send();
+  }
+  return -1;
+}
+const readStream = () => {
+  reader.read().then(({ done, value }) => {
+    if (done) {
+      // 如果流已经结束，返回
+      return;
+    }
+    // 否则，继续处理数据
+    const data = new Uint8Array(value);
+    const index = indexOfSOI(data);
+    if (index > -1) {
+      const blob = new Blob([data.slice(index)], {
+        type: "image/jpeg",
+      });
+      stream = URL.createObjectURL(blob);
+      document.getElementById('img').src = stream;
+    }
+    // 再次调用自身函数，继续读取数据
+    readStream();
+  });
 }
 
 // @ is an alias to /src
@@ -66,7 +100,21 @@ onMounted(() => {
   // }).catch((err) => {
   //   console.log(err)
   // })
-  nbMethod()
+  // nbMethod()
+  fetch("http://152.136.213.16:8001/video/pull?url=rtmp://152.136.213.16:1935/live/test")
+    .then((response) => {
+      // 获取可读流
+      const stream = response.body;
+      console.log(stream)
+      // 获取读取器
+      reader = stream.getReader();
+      // 递归地从流中读取数据
+      readStream();
+    })
+    .catch((error) => {
+      // 处理错误情况
+      console.error(error);
+    });
 
 
   // var ctx = document.getElementById('test_canvas').getContext('2d');
